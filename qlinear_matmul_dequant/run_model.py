2d8de206409f325bfd7881e0fe2cd8abdc83e242
mounikk <mounikk@XHDMOUNIKK01>
2025-02-20 14:42:00 +0530

    updated with run_model script

---
+++ qlinear_matmul_dequant/run_model.py
@@ -0,0 +1,10 @@
+import onnxruntime as ort
+
+session = ort.InferenceSession("quant_dequant.onnx")
+input_name = session.get_inputs()[0].name
+output_name = session.get_outputs()[0].name
+
+input_data = np.random.rand(1, 10).astype(np.float32)  # Generate random FP32 input
+output = session.run([output_name], {input_name: input_data})
+
+print("Output:", output)
